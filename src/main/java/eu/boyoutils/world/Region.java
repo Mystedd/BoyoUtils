63dffc0090dd9399d303b54928204082f4759139
Myst3d <kianbartley07@gmail.com>
2023-03-07 18:07:32 +0000

    Added Region.java

---
+++ src/main/java/eu/boyoutils/world/Region.java
@@ -0,0 +1,20 @@
+package eu.boyoutils.world;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import java.util.logging.Logger;
+
+public class Region {
+
+    final Logger logger = Bukkit.getLogger();
+
+    Location firstCorner;
+    Location secondCorner;
+
+    public Region(Location firstCorner, Location secondCorner) {
+
+
+
+    }
+
+}
